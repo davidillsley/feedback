2ff32e8bdb6bae2f1f6ca3312247b606c67db3b8
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-09-05 15:11:29 +0000

    Creating a page improvement responds with JSON

--- app/controllers/contact/govuk/page_improvements_controller.rb
+++ app/controllers/contact/govuk/page_improvements_controller.rb
@@ -2,7 +2,7 @@ class Contact::Govuk::PageImprovementsController < ApplicationController
   def create
     Feedback.support_api.create_page_improvement(page_improvements_params)
 
-    render nothing: true
+    render json: { status: 'success' }
   end
 
   def page_improvements_params
--- spec/requests/page_improvements_spec.rb
+++ spec/requests/page_improvements_spec.rb
@@ -4,14 +4,33 @@ require 'gds_api/test_helpers/support_api'
 describe "Page improvements" do
   include GdsApi::TestHelpers::SupportApi
 
+  let(:common_headers) { { "CONTENT_TYPE" => "application/json" } }
+
   it "submits the feedback to the Support API" do
     stub_any_support_api_call
 
-    post "/contact/govuk/page_improvements", description: "The title is the wrong colour."
+    post "/contact/govuk/page_improvements",
+      { description: "The title is the wrong colour." }.to_json,
+      common_headers
 
     expected_request = a_request(:post, Plek.current.find('support-api') + "/page-improvements")
       .with(body: { "description" => "The title is the wrong colour." })
 
     expect(expected_request).to have_been_made
+  end
+
+  it "responds successfully" do
+    stub_any_support_api_call
+
+    post "/contact/govuk/page_improvements",
+      { description: "The title is the wrong colour." }.to_json,
+      common_headers
+
+    assert_response :success
+    expect(response_hash).to include('status' => 'success')
+  end
+
+  def response_hash
+    JSON.parse(response.body)
   end
 end
