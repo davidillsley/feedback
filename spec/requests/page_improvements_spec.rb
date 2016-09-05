da9c6a1d65e07a75db2a97222f09e526320c4a67
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-09-05 16:02:14 +0000

    Return a JSON error response if the Support API is gone

--- spec/requests/page_improvements_spec.rb
+++ spec/requests/page_improvements_spec.rb
@@ -4,7 +4,7 @@ require 'gds_api/test_helpers/support_api'
 describe "Page improvements" do
   include GdsApi::TestHelpers::SupportApi
 
-  let(:common_headers) { { "CONTENT_TYPE" => "application/json" } }
+  let(:common_headers) { { "Accept" => "application/json", "Content-Type" => "application/json" } }
 
   it "submits the feedback to the Support API" do
     stub_any_support_api_call
@@ -28,6 +28,19 @@ describe "Page improvements" do
 
     assert_response :success
     expect(response_hash).to include('status' => 'success')
+  end
+
+  context "when the Support API isn't available" do
+    it "responds with an error" do
+      support_api_isnt_available
+
+      post "/contact/govuk/page_improvements",
+        { description: "The title is the wrong colour." }.to_json,
+        common_headers
+
+      assert_response :error
+      expect(response_hash).to include('status' => 'error')
+    end
   end
 
   def response_hash
